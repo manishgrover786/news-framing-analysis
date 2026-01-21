435900ecbbb3fd71178ff90b7b95ef5b6212a9da
manishnewlap <manishgrover@MANISHASUS>
2026-01-21 10:59:53 -0600

    initial structure, rss links, first prompt topic

---
+++ src/rss_sources.py
@@ -0,0 +1,5 @@
+RSS_FEEDS = {
+    "NYTimes": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "CNN": "https://rss.cnn.com/rss/edition.rss",
+    "FoxNews": "https://feeds.foxnews.com/foxnews/latest"
+}
